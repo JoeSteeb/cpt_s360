b99170db4cb45aa46998fb39cdac9b3d58164443
JoeSteeb <josteeb@gmail.com>
2021-09-30 08:15:12 -0700

    started on pipe

--- lab3/main.c
+++ lab3/main.c
@@ -49,6 +49,50 @@ int checkdir(char *pathname, char *filename)
     return 0;
 }
 
+int pipeIt(char *program1, char *program2)
+{
+    int fd[2];
+    if (pipe(fd) == -1)
+    {
+        return 1;
+    }
+
+    int pid1 = fork();
+    if (pid1 < 0)
+    {
+        return 2;
+    }
+    if (pid1 == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execlp("cat", "cat", "/home/j/code/cs360/tests/pipe/main.c", NULL);
+    }
+
+    int pid2 = fork();
+
+    if (pid2 < 0)
+    {
+        return 3;
+    }
+
+    if (pid2 == 0)
+    {
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execlp("grep", "grep", "main", NULL);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+
+    waitpid(pid2, NULL, 0);
+    waitpid(pid1, NULL, 0);
+    return 0;
+}
+
 int main(int argc, char *argv[], char *env[])
 {
     int i;
